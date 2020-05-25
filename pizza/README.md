5c4edbf9146ace77db5672ac2c75e2c6b37ca4dd
Andrewka-Java <alze.andrey.1997@mail.ru>
2020-05-26 01:49:46 +0300

    first commit

--- pizza/README.md
+++
@@ -1,5 +0,0 @@
-Swagger-api
->   http://localhost:8088/v2/api-docs
-
-Swagger-ui
->   localhost:8088/swagger-ui.html
--- pizza/target/classes/public/js/main.js
+++
@@ -1,9 +0,0 @@
-let app = angular.module("pizza", []);
-
-app.controller("AppCtrl", function($scope, $http) {
-    $scope.titles = [];
-
-    $http.get("http://localhost:8088/api/pizzas").success(function (data) {
-        $scope.titles = data;
-    })
-});
